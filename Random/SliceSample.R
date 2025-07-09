e7de40af93bc2f4d7eae076853c50780ba77c41d
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2025-07-09 13:33:05 -0400

    Update SliceSample.R

--- Random/SliceSample.R
+++ Random/SliceSample.R
@@ -4,3 +4,17 @@ df_Subset <- df_all %>%
   group_by(label) %>%
   slice_sample(n = 1000) %>%
   ungroup()
+
+#To group based on same values in column "GENE". Values in colum Trait will be collapsed using ",". Only the minimum P-Values in column 'P' will be reported
+library(dplyr)
+
+library(dplyr)
+
+df_collapsed <- df %>%
+  group_by(Gene) %>%
+  summarise(
+    Trait = paste(unique(Trait), collapse = ","),
+    Trait2     = paste(unique(Trait2), collapse = ","),
+    P     = min(P, na.rm = TRUE),
+    .groups = "drop"
+  )
