63f95de593b8c6610c2c89e6768ee4677663d873
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2026-05-10 15:19:41 -0400

    Update git_codes.sh

--- Github_push/git_codes.sh
+++ Github_push/git_codes.sh
@@ -1,2 +1,14 @@
 git status
 git log
+git remote add origin git@github.com:yourRepository.git
+git remote 
+git remote -v
+
+git push -u orign main #pushing to main branch
+ssh-keygen -t rsa -b 4096 -C "emailAddress@gmail.com" #generates public private key pare in your .ssh/id_rsa.pub
+ls ~/.ssh/id_rsa*
+cat ~/.ssh/id_rsa*.pub
+Put this into github into SSH and GPG keys.
+then do:
+git push -u orign main #pushing to main branch and all changes pushed to github
+
